4d4647a0fe1eaaef40d3c30b5f4ab9d9fd0e8fb3
rathomit <rathomit@sheridancollege.ca>
2023-01-16 15:27:29 -0500

    week 2 student list

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) 
+    {
+        //store and print the student listjgjg
+        Student[] list = new Student[3];  //declaration Array of Object
+        Student s1 = new Student();
+        s1.setName("Yesha");  //s1 = object
+        s1.setAge(19);
+        
+        Student s2 = new Student();
+        s2.setName("Mitali");  //s1 = object
+        s2.setAge(20);
+        
+        Student s3 = new Student();
+        s3.setName("Arsh");  //s1 = object
+        s3.setAge(30);
+        
+        list[0]=s1;
+        list[1]=s2;
+        list[2]=s3;
+        
+        for(int i=0;i<list.length;i++)
+        {
+            System.out.println(list[i].getName() + " " + list[i].getAge());
+        }
+    }
+    
+}
