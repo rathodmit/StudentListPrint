edd9f7751ca83ac3dfc5a85c0e9003a45206d81a
rathodmit <122311003+rathodmit@users.noreply.github.com>
2023-01-23 14:31:52 -0500

    Update JavaApplication3.java

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -30,6 +30,7 @@ public class JavaApplication3 {
         for(int i=0;i<list.length;i++)
         {
             System.out.println(list[i].getName() + " " + list[i].getAge());
+            System.out.println();
         }
     }
     
